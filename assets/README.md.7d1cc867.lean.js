95865976d291a6a121acb9bb9de2746f40a0b93d
mmdjiji <mmdjiji@users.noreply.github.com>
2022-08-19 06:37:12 +0000

    deploy: f2c0102374511b475a3cd182f4dba8016b9566d6

---
+++ assets/README.md.7d1cc867.lean.js
@@ -0,0 +1,4 @@
+import{_ as a,f as e,e as s,N as n}from"./plugin-vue_export-helper.147b70e9.js";const f='{"title":"Docs","description":"","frontmatter":{},"headers":[{"level":2,"title":"Dev","slug":"dev"}],"relativePath":"README.md","lastUpdated":1660891020160}',t={},c=n(`__VP_STATIC_START__<h1 id="docs"><a class="header-anchor" href="#docs" aria-hidden="true">#</a> Docs</h1><h2 id="dev"><a class="header-anchor" href="#dev" aria-hidden="true">#</a> Dev</h2><div class="language-bash"><pre><code>$ <span class="token function">git</span> clone https://github.com/jmpsvr/docs <span class="token operator">&amp;&amp;</span> <span class="token builtin class-name">cd</span> docs
+$ <span class="token function">yarn</span>
+$ <span class="token function">yarn</span> dev
+</code></pre></div>__VP_STATIC_END__`,3),o=[c];function r(d,i,p,_,l,h){return s(),e("div",null,o)}var v=a(t,[["render",r]]);export{f as __pageData,v as default};
